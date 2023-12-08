aa40630e6ead25ce5712f9686bd6698d0d420708
amaan <amaankhan9431@gmail.com>
2023-12-09 00:48:13 +0530

    hlo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <body>
+        <h1>hello world</h1>
+      </body>
+    </html>
+  );
+}
+
+export default App;
